55928a4d688c9458b31dcd9e86c3c465e48cb324
zhoupengzu <zhoupengzu@maizijf.com>
2018-07-16 17:45:49 +0800

    添加入口

--- ZPZQuickProject/ZPZQuickProject/ZPZAppDelegate.m
+++ ZPZQuickProject/ZPZQuickProject/ZPZAppDelegate.m
@@ -23,22 +23,25 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     _window.backgroundColor = [UIColor whiteColor];
-    
+    _rootTabBar = [[ZPZBaseTabBarViewController alloc] init];
+    _rootNav = [[ZPZBaseNavigationViewController alloc] init];
+    [_rootTabBar addChildViewController:_rootNav];
+    _window.rootViewController = _rootTabBar;
     // 如果是第一次启动，则不再去请求广告；如果不是第一次启动，则去请求是否需要展示广告
-    if ([ZPZPublicManager isFirstLaunch]) {
-        // 第一次启动
-        [ZPZPublicManager recordFirstLaunch];
-        // 启动引导页展示
-        ZPZGuideViewController * guideVC = [[ZPZGuideViewController alloc] init];
-        _window.rootViewController = guideVC;
-    } else {
-        //        _rootTabBar = [[MYBaseTabBarViewController alloc] init];
-        //        _rootNav = [[MYBaseNavigationViewController alloc] init];
-        //        [_rootTabBar addChildViewController:_rootNav];
-        ZPZADViewController * adVC = [[ZPZADViewController alloc] init];
-        _window.rootViewController = adVC;
-        //        [self requestAndShowAD];
-    }
+//    if ([ZPZPublicManager isFirstLaunch]) {
+//        // 第一次启动
+//        [ZPZPublicManager recordFirstLaunch];
+//        // 启动引导页展示
+//        ZPZGuideViewController * guideVC = [[ZPZGuideViewController alloc] init];
+//        _window.rootViewController = guideVC;
+//    } else {
+//        //        _rootTabBar = [[MYBaseTabBarViewController alloc] init];
+//        //        _rootNav = [[MYBaseNavigationViewController alloc] init];
+//        //        [_rootTabBar addChildViewController:_rootNav];
+//        ZPZADViewController * adVC = [[ZPZADViewController alloc] init];
+//        _window.rootViewController = adVC;
+//        //        [self requestAndShowAD];
+//    }
     [_window makeKeyAndVisible];
     return YES;
 }
